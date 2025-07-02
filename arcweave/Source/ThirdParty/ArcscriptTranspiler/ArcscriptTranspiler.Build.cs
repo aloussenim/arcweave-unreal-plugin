c530dedefabde0357eb018675dd4d08d698387d1
aloussenim <165428230+aloussenim@users.noreply.github.com>
2025-07-02 16:47:08 -0400

    Updated paths to not rely on the environment variables.

--- arcweave/Source/ThirdParty/ArcscriptTranspiler/ArcscriptTranspiler.Build.cs
+++ arcweave/Source/ThirdParty/ArcscriptTranspiler/ArcscriptTranspiler.Build.cs
@@ -6,31 +6,37 @@ public class ArcscriptTranspiler : ModuleRules
     public ArcscriptTranspiler(ReadOnlyTargetRules Target) : base(Target)
     {
         Type = ModuleType.External;
+
+        string ThirdPartyBasePath = Path.Combine(ModuleDirectory, "Source", "ThirdParty", "ArcscriptTranspiler");
+
         if (Target.Platform == UnrealTargetPlatform.Win64)
         {
             // Add the import library
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "antlr4-runtime.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ArcscriptTranspiler.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyBasePath, "x64", "Release", "antlr4-runtime.lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyBasePath, "x64", "Release", "ArcscriptTranspiler.lib"));
+
             // Delay-load the DLL, so we can load it from the right place first
             PublicDelayLoadDLLs.Add("antlr4-runtime.dll");
             PublicDelayLoadDLLs.Add("ArcscriptTranspiler.dll");
             // Ensure that the DLL is staged along with the executable
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/x64/Release/antlr4-runtime.dll");
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/x64/Release/ArcscriptTranspiler.dll");
+            RuntimeDependencies.Add(Path.Combine(ThirdPartyBasePath, "x64", "Release", "antlr4-runtime.dll"));
+            RuntimeDependencies.Add(Path.Combine(ThirdPartyBasePath, "x64", "Release", "ArcscriptTranspiler.dll"));
         }
         else if (Target.Platform == UnrealTargetPlatform.Mac)
         {
             // Ensure that the DLL is staged along with the executable
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/Mac/Release/libantlr4-runtime.dylib");
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/Mac/Release/libArcscriptTranspiler.dylib");
+            // Use Path.Combine with ModuleDirectory or ThirdPartyBasePath
+            RuntimeDependencies.Add(Path.Combine(ThirdPartyBasePath, "Mac", "Release", "libantlr4-runtime.dylib"));
+            RuntimeDependencies.Add(Path.Combine(ThirdPartyBasePath, "Mac", "Release", "libArcscriptTranspiler.dylib"));
         }
 
+        // PublicIncludePaths should also use Path.Combine with the ModuleDirectory or ThirdPartyBasePath
         PublicIncludePaths.AddRange(new string[]
         {
-            "$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/antlr4-runtime/src",
-            "$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/ArcscriptTranspiler/src",
-            "$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/ArcscriptTranspiler/src/Generated/ArcscriptParser",
-            "$(PluginDir)/Source/ThirdParty/ArcscriptTranspiler/ArcscriptTranspiler/src/Generated/ArcscriptLexer"
+            Path.Combine(ThirdPartyBasePath, "antlr4-runtime", "src"),
+            Path.Combine(ThirdPartyBasePath, "ArcscriptTranspiler", "src"),
+            Path.Combine(ThirdPartyBasePath, "ArcscriptTranspiler", "src", "Generated", "ArcscriptParser"),
+            Path.Combine(ThirdPartyBasePath, "ArcscriptTranspiler", "src", "Generated", "ArcscriptLexer")
         });
 
         PublicDependencyModuleNames.AddRange(new string[] {
